588d9af4cfee5184343051cc01df8a4b34a71473
Linxiangyu <lxyweb@gmail.com>
2013-02-05 03:08:07 +0800

    fisrt commit

---
+++ README.md
@@ -0,0 +1,19 @@
+
+#程序员老皇历
+
+![img](http://ww1.sinaimg.cn/large/a74e55b4jw1e1hzabefhij.jpg)
+
+这个东西最早来自[这里](http://sandbox.runjs.cn/show/ydp3it7b)
+
+觉得挺有意思。。而且具体实现赤裸裸的写在页面的JS里面。。也没什么。。想移植到Android。。
+
+然后感觉有点麻烦。。懒得写了。。用傻办法5分钟搞定。。其实就是让Android显示一个本地网页。。
+
+- 新建项目
+- 修改布局文件，加入一个长宽都是match_parent的WebView
+- 默认Activity里，加载一个本地网页到WebView
+- 把那个网页和需要的JQuery保存到Android本地
+- Done。
+
+然后花了好久找Mac下的图片编辑软件。。算了，随便弄了一个(DEC25 == OCT31)的文字当Logo。。不会PS的码农伤不起。。
+
---
+++ src/com/example/CoderCalendar/MyActivity.java
@@ -0,0 +1,30 @@
+package com.example.CoderCalendar;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.webkit.*;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.VideoView;
+
+public class MyActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+
+
+        WebView webView = (WebView) findViewById(R.id.webView);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+
+        webView.loadUrl( "file:///android_asset/index.html");
+
+    }
+}
